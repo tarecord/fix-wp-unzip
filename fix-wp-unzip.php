3c7d21dc59765ccc55fedbd44c1b58f57a85611d
Tanner Record <tanner.record@gmail.com>
2024-03-03 14:08:58 -0500

    Initial commit

---
+++ fix-wp-unzip.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Fix Zip Uploads
+ * Description:       A simple utility plugin that fixes the zip unarchive issue introduced in WordPress 6.4
+ * Version:           1.0.0
+ * Requires at least: 6.4.3
+ * Requires PHP:      7.2
+ * Author:            Tanner Record
+ * Author URI:        https://www.tannerrecord.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        fix-wp-unzip
+ */
+
+// @see https://core.trac.wordpress.org/ticket/60398#trac-change-10-1706712301303331
+add_filter( 'unzip_file_use_ziparchive', '__return_false' );
